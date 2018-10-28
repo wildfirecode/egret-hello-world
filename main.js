8033e7d4a76bb6e6c7237ab4fecab4b02b0f81eb
wildfirecode <wildfirecode13@gmail.com>
2018-10-28 11:03:10 +0800

    update

--- main.js
+++ main.js
@@ -11,15 +11,5 @@ class Main extends egret.DisplayObjectContainer {
         textfield.text = 'hello world';
     }
 }
-window['Main']=Main;
-egret.runEgret({
-    renderMode: "webgl", audioType: 0, calculateCanvasScaleFactor: function (context) {
-        var backingStore = context.backingStorePixelRatio ||
-            context.webkitBackingStorePixelRatio ||
-            context.mozBackingStorePixelRatio ||
-            context.msBackingStorePixelRatio ||
-            context.oBackingStorePixelRatio ||
-            context.backingStorePixelRatio || 1;
-        return (window.devicePixelRatio || 1) / backingStore;
-    }
-});
+window['Main'] = Main;
+egret.runEgret();
